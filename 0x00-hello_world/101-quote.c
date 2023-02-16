cf5d3d74c4c9d9e3fd6cdfb15c43433c482e7812
aymen <ayimen69wondimak@gmail.com>
2023-02-16 11:59:51 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return 1 (success)
+ * Return: 1
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
- 
