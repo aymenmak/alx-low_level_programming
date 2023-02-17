3f0ac27c6868d3f21e93f159e2a0e93ff3ef7ebc
aymen <ayimen69wondimak@gmail.com>
2023-02-17 04:28:37 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,18 +6,23 @@
  */
 
 int main(void)
-{       
-        int a;
-        
-        for (a =0; a < 10; a++)
-        {
-                putchar(a + '0');
-                if (a < 9)
-                {
-                        putchar(',');
-			putchar(' ');
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
 		}
-	}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
