bfabf33d8fb3d60d5c023d6f3351b24d3940cb4c
aymen <ayimen69wondimak@gmail.com>
2023-02-21 12:10:28 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computers that absolute value of an integer
+ * @t: The intiger to be computed
+ * Return: The absolute value
+ */
+int _abs(int t)
+{       
+        
+        if (t >= 0)
+                return (t);
+        else
+                return (-t);
+}
