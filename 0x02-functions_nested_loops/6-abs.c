81c2cefa696718253b858d41621eab1a8f2c7a26
aymen <ayimen69wondimak@gmail.com>
2023-02-22 08:06:58 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,12 @@
 /**
  * _abs - Computers that absolute value of an integer
  * @t: The intiger to be computed
- * Return: The absolute value
+ * Return: The absolut value
  */
 int _abs(int t)
-{       
-        
-        if (t >= 0)
-                return (t);
-        else
-                return (-t);
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
 }
