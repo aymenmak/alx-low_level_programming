afec174173a1e7f90d7ef10bbe257e6cad245b2c
aymen <ayimen69wondimak@gmail.com>
2023-02-23 13:47:31 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf_diagonal(0);
+	printf_diagonal(2);
+	printf_diagonal(10);
+	printf_diagonal(-1);
+	return (0);
+}
