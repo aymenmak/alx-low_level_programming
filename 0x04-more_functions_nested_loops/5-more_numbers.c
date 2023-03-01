30d411cad8b9aa8931e2422f8bf58d1f404ac8f2
aymen <ayimen69wondimak@gmail.com>
2023-03-01 06:11:31 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,6 +6,7 @@
  */
 
 void more_numbers(void)
+
 {
 
 	int x, y;
@@ -16,9 +17,9 @@ void more_numbers(void)
 		{
 			if (y > 0)
 			{
-				_putchar[(y / 10) + '0'];
+				_putchar((y / 10) + '0');
 			}
-			_putchar[(y % 10) + '0'];
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
