f255605891af65f931a39654729b547e493ee68d
aymen <ayimen69wondimak@gmail.com>
2023-03-01 06:07:25 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,11 +2,12 @@
 
 /**
  * more_numbers - Print 10 times the numbers since 0 up to 14
- * Return: 10times of the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
 {
+
 	int x, y;
 
 	for (x = 0; x < 10; x++)
@@ -15,9 +16,9 @@ void more_numbers(void)
 		{
 			if (y > 0)
 			{
-				_putchar((y / 10) + '0');
+				_putchar[(y / 10) + '0'];
 			}
-			_putchar((y % 10) + '0');
+			_putchar[(y % 10) + '0'];
 		}
 		_putchar('\n');
 	}
