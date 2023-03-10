9dc41e5619dc2de43a2e50f7b64662c6bccc8c14
aymen <ayimen69wondimak@gmail.com>
2023-03-10 13:08:03 -0800

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -37,12 +37,9 @@ int _atoi(char *s)
 
 		len++;
 
-
-
-while (i < len && f == 0)
+	while (i < len && f == 0)
 
 	{
-
 		if (s[i] == '-')
 
 			++d;
@@ -56,18 +53,23 @@ while (i < len && f == 0)
 			digit = s[i] - '0';
 
 			if (d % 2)
-
+			{
 				digit = -digit;
 
 			n = n * 10 + digit;
 
 			f = 1;
 
+			}
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+			{
 
 				break;
 
 			f = 0;
+			}
 
 		}
 
